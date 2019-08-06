f179c6ad3b5b23302f9245bc47835cd96a10af94
Benard Obinna <bernardobinna2@gmail.com>
2019-08-06 21:53:32 +0100

    added the categories array

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ var app = new Vue({
     message: "Hello Vue!",
     status: false,
     // false on the status is single
-    gender: false
+    gender: false,
     // false on the gender is male
+
+    client: "John wick",
+    categories: [
+      { name: "Groceries & Supplies", amount: 30000 },
+      { name: "Transportation", amount: 25000 },
+      { name: "Bills & Utilities", amount: 30000 },
+      { name: "Healthcare", amount: 30000 },
+      { name: "Housing", amount: 30000 },
+      { name: "Personal Care", amount: 30000 },
+      { name: "Savings & investments", amount: 30000 },
+      { name: "Family & dependants", amount: 30000 },
+      { name: "Miscellaneous", amount: 30000 },
+      { name: "Emergency funds", amount: 30000 }
+    ]
   },
 
   methods: {
